89b0deabbdbb7d09cba881449a73de33eeb10347
Steven Ly <steven.ly236@gmail.com>
2025-02-01 20:55:06 -0800

    utils

---
+++ labs/3/modules/utils.js
@@ -0,0 +1,9 @@
+const messages = require('../lang/en/en.js');
+
+function getDate(name) {
+    const currentTime = new Date().toString(); 
+    const message = messages.GREETING.replace('%1', name).replace('%2', currentTime);
+    return `<p style="color:blue;">${message}</p>`; 
+}
+
+module.exports = { getDate };
