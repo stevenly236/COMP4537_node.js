b565cf570b190a9e55292735785fee238afa1784
Steven Ly <steven.ly236@gmail.com>
2025-02-08 15:07:02 -0800

    fix port

--- labs/4/app.js
+++ labs/4/app.js
@@ -94,6 +94,9 @@ class DictionaryAPI {
 }
 
 const dictionaryAPI = new DictionaryAPI();
+
+const PORT = process.env.PORT || 3000;
+
 const server = http.createServer((req, res) => dictionaryAPI.handleRequest(req, res));
 
-server.listen(3000, () => console.log("Server is running on port 3000"));
+server.listen(PORT, () => console.log("Server is running on port 3000"));
