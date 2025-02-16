d2096fe93a5c05c921c40becca204d6ebfd99b5c
Steven Ly <steven.ly236@gmail.com>
2025-02-16 14:54:06 -0800

    .env

--- labs/5/server2/server2.js
+++ labs/5/server2/server2.js
@@ -12,9 +12,9 @@ class DatabaseServer {
             user: process.env.DB_USER,
             password: process.env.DB_PASSWORD,
             database: process.env.DB_NAME,
-            port: 3306,
+            port: process.env.DB_PORT || 25060,
             ssl: {
-                rejectUnauthorized: false
+                rejectUnauthorized: true
             }
         };
         
