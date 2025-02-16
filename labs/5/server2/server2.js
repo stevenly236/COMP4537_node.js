7443230a68750df17356c8d4b7801bf49daf1be3
Steven Ly <steven.ly236@gmail.com>
2025-02-16 15:54:39 -0800

    cors

--- labs/5/server2/server2.js
+++ labs/5/server2/server2.js
@@ -48,7 +48,7 @@ class DatabaseServer {
     }
 
     async handleRequest(req, res) {
-        res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+        res.setHeader('Access-Control-Allow-Origin', '*');
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
         res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
